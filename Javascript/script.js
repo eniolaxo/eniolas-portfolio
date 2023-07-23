79107946b24d90505e1bed9e7472364c07d25c37
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-07-23 16:45:27 +0100

    removed carousel, added hyperlink of cv in button

---
+++ Javascript/script.js
@@ -0,0 +1,30 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll(".section");
+  let currentSection = 0;
+  let isScrolling = false;
+
+  function scrollToSection(index) {
+    if (index < 0 || index >= sections.length) return;
+
+    const targetSection = sections[index];
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  }
+
+  function handleScroll() {
+    if (!isScrolling) {
+      isScrolling = true;
+
+      // Wait for 100ms to prevent multiple scrolls firing at once
+      setTimeout(function () {
+        const delta = (window.innerHeight * 0.7) / Math.abs(window.innerHeight * 0.7);
+        currentSection -= delta;
+        scrollToSection(Math.round(currentSection));
+
+        isScrolling = false;
+      }, 100);
+    }
+  }
+
+  window.addEventListener("wheel", handleScroll);
+});
