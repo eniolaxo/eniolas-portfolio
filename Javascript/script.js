3e8d543a690919220a47639e274c464887f41218
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-07-24 00:55:48 +0100

    making aesthetic changes

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,30 +1,39 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll(".section");
-  let currentSection = 0;
-  let isScrolling = false;
+// // script.js
+// document.addEventListener("DOMContentLoaded", function () {
+//   const sections = document.querySelectorAll(".section");
+//   let currentSection = 0;
+//   let isScrolling = false;
 
-  function scrollToSection(index) {
-    if (index < 0 || index >= sections.length) return;
+//   function scrollToSection(index) {
+//     if (index < 0 || index >= sections.length) return;
 
-    const targetSection = sections[index];
-    targetSection.scrollIntoView({ behavior: "smooth" });
-  }
+//     const targetSection = sections[index];
+//     targetSection.scrollIntoView({ behavior: "smooth" });
+//   }
 
-  function handleScroll() {
-    if (!isScrolling) {
-      isScrolling = true;
+//   function handleScroll() {
+//     if (!isScrolling) {
+//       isScrolling = true;
 
-      // Wait for 100ms to prevent multiple scrolls firing at once
-      setTimeout(function () {
-        const delta = (window.innerHeight * 0.7) / Math.abs(window.innerHeight * 0.7);
-        currentSection -= delta;
-        scrollToSection(Math.round(currentSection));
+//       // Wait for 100ms to prevent multiple scrolls firing at once
+//       setTimeout(function () {
+//         const delta = (window.innerHeight * 0.7) / Math.abs(window.innerHeight * 0.7);
+//         currentSection -= delta;
+//         scrollToSection(Math.round(currentSection));
 
-        isScrolling = false;
-      }, 100);
-    }
-  }
+//         isScrolling = false;
+//       }, 100);
+//     }
+//   }
 
-  window.addEventListener("wheel", handleScroll);
-});
+//   window.addEventListener("wheel", handleScroll);
+// });
+
+
+// const menuIcon = document.querySelector(".menu-icon");
+// const menuItems = document.querySelector(".menu-items");
+
+// menuIcon.addEventListener("click", () => {
+//   menuItems.classList.toggle("collapsed");
+// });
+
